5403a3d989f13f6c4a8d49e413ca553cff32e188
AK <aamaal00777@gmail.com>
2026-03-10 17:52:05 +0400

    Header and footer configuration done

--- main.c
+++ main.c
@@ -1,38 +1,67 @@
 #include <ncurses.h>
+#include <string.h>
 
 int main()
 {
     initscr();
     start_color();
     keypad(stdscr, TRUE);
+    noecho();
+    curs_set(0);
 
     init_pair(1, COLOR_BLACK, COLOR_CYAN);
     init_pair(2, COLOR_BLACK, COLOR_GREEN);
 
-    int rows, cols;
-    getmaxyx(stdscr, rows, cols);
-
     int ch;
+    int leftPos = 0;
+
+    // Header texts
+    char* headerLeftText = "Ln 12 Col 5";
+    char* headerMiddleText = "lucrativeText";
+    char* headerRightText = "filename.txt";
+
+    // Footer texts
+    char* footerLeftText = "Ctrl+Q to quit";
+    char* footerMiddleText = "Ctrl+S to save";
+    char* footerRightText = "Ctrl+W to close without save";
 
     while(1)
     {
+        int rows, cols;
+        getmaxyx(stdscr, rows, cols);
+
+        int headerMiddlePos = (cols / 2) - (strlen(headerMiddleText)/2);
+        int headerRightPos  = cols - strlen(headerRightText) - 1;
+        int footerMiddlePos = (cols / 2) - (strlen(footerMiddleText)/2);
+        int footerRightPos  = cols - strlen(footerRightText) - 1;
+
         clear();
 
-        attron(COLOR_PAIR(1));
-        mvprintw(0, 0, " AK Editor ");
-        attroff(COLOR_PAIR(1));
+        // Header
+        attron(COLOR_PAIR(1) | A_BOLD | A_UNDERLINE);
+        mvprintw(0, leftPos, "%s", headerLeftText);
+        mvprintw(0, headerMiddlePos, "%s", headerMiddleText);
+        mvprintw(0, headerRightPos, "%s", headerRightText);
+        attroff(COLOR_PAIR(1) | A_BOLD | A_UNDERLINE);
 
-        attron(COLOR_PAIR(2));
-        mvprintw(rows-1, 0, " Press q to quit ");
-        attroff(COLOR_PAIR(2));
+        // Footer
+        attron(COLOR_PAIR(2) | A_BOLD);
+        mvprintw(rows-1, leftPos, "%s", footerLeftText);
+        mvprintw(rows-1, footerMiddlePos, "%s", footerMiddleText);
+        mvprintw(rows-1, footerRightPos, "%s", footerRightText);
+        attroff(COLOR_PAIR(2) | A_BOLD);
 
         refresh();
 
         ch = getch();
 
-        if(ch == 'q')
+        if(ch == 17)
             break;
+
+        if(ch == 18)
+            continue;
     }
 
     endwin();
+    return 0;
 }
